32d7baa4dfddc2e58104636c846062cc31ae0755
papanugget <evilgenius04@gmail.com>
2018-06-19 09:11:04 -0400

    Got playfield to render

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,21 +26,37 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function (){
-
+const Player = function (x, y, speed){
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
 };
-Player.prototype.update() = function() {
-
+Player.prototype.update = function() {
+    // console.log('player update prototype called');
 }; 
-Player.prototype.render() = function() {
-
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-Player.prototype.handleInput() = function() {
-
+Player.prototype.handleInput = function(e) {
+    console.log(e);
 };
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+//position player at location
+const player = new Player(200,380,50);
+
+// Place all enemy objects in an array called allEnemie
 // Place the player object in a variable called player
+const allEnemies = [];
+//position enemies at first blocks of the yAxis
+const enemyLoc = [60,140,220];
+enemyLoc.forEach(function(yAxis) {
+    const enemy = new Enemy(0, yAxis, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
+
+
+console.log(allEnemies);
 
 
 
