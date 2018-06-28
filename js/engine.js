6174f7d7d76bc4c5f1c35d6761df3b2b805ecbc4
papanugget <evilgenius04@gmail.com>
2018-06-28 15:26:27 -0400

    removed reference to unused asset

--- js/engine.js
+++ js/engine.js
@@ -171,8 +171,7 @@ var Engine = (function(global) {
         './images/water-block.png',
         './images/grass-block.png',
         './images/enemy-bug.png',
-        './images/char-boy.png',
-        './images/explode.png'
+        './images/char-boy.png'
     ]);
     Resources.onReady(init);
 
